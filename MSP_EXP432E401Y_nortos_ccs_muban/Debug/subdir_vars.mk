289e97c167731c4f6707a56ddd6285e0753717d0
xiaokamikami <38394607+xiaokamikami@users.noreply.github.com>
2021-06-28 19:01:47 +0800

    E401Y,Y401R模板

---
+++ MSP_EXP432E401Y_nortos_ccs_muban/Debug/subdir_vars.mk
@@ -0,0 +1,35 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../msp432e401y.cmd 
+
+C_SRCS += \
+../gpio_bitbanded_output.c \
+../system_msp432e401y.c 
+
+C_DEPS += \
+./gpio_bitbanded_output.d \
+./system_msp432e401y.d 
+
+OBJS += \
+./gpio_bitbanded_output.obj \
+./system_msp432e401y.obj 
+
+OBJS__QUOTED += \
+"gpio_bitbanded_output.obj" \
+"system_msp432e401y.obj" 
+
+C_DEPS__QUOTED += \
+"gpio_bitbanded_output.d" \
+"system_msp432e401y.d" 
+
+C_SRCS__QUOTED += \
+"../gpio_bitbanded_output.c" \
+"../system_msp432e401y.c" 
+
+
---
+++ MSP_EXP432P401R__ccs_muban/Debug/subdir_vars.mk
@@ -0,0 +1,41 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../msp432p401r.cmd 
+
+C_SRCS += \
+../gpio_toggle_output.c \
+../pwmled.c \
+../system_msp432p401r.c 
+
+C_DEPS += \
+./gpio_toggle_output.d \
+./pwmled.d \
+./system_msp432p401r.d 
+
+OBJS += \
+./gpio_toggle_output.obj \
+./pwmled.obj \
+./system_msp432p401r.obj 
+
+OBJS__QUOTED += \
+"gpio_toggle_output.obj" \
+"pwmled.obj" \
+"system_msp432p401r.obj" 
+
+C_DEPS__QUOTED += \
+"gpio_toggle_output.d" \
+"pwmled.d" \
+"system_msp432p401r.d" 
+
+C_SRCS__QUOTED += \
+"../gpio_toggle_output.c" \
+"../pwmled.c" \
+"../system_msp432p401r.c" 
+
+
